f0346a0073d5389c4bf258eac2fb5ec6cddb118c
MaxFas <FastsovMax@yandex.ru>
2021-03-21 13:07:23 +0300

    fix display

--- src/components/result/display/Display.tsx
+++ src/components/result/display/Display.tsx
@@ -11,8 +11,10 @@ export type DisplayType = {
 export function Display(props: DisplayType) {
     return (
         <div className={"display"}>
-           <div className={props.value === props.maxValue||props.errorMin||props.errorMax? 'resultMax': 'result'} >
-               {props.errorMin||props.errorMax? 'Incorrect value' : props.value}</div>
+            {props.errorMax||props.errorMin? <div className={'incorrectValue'}>
+                {'Incorrect value!'}</div>
+                :<div className={props.value === props.maxValue ? 'resultMax' : 'result'} >
+                {props.errorMin || props.errorMax ? 'Incorrect value' : props.value}</div>}
         </div>
     );
 }
